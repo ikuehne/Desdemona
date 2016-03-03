e7e5dc9d7a088b87c91d11d811262858c4edaf1d
ikuehne <ikuehne617@gmail.com>
2016-03-02 23:09:51 -0800

    Added Doxygen-style headers and a Doxygen configuration.
    Added headers to the Player class and a very simple (default) config.

--- player.cpp
+++ player.cpp
@@ -1,10 +1,5 @@
 #include "player.h"
 
-/*
- * Constructor for the player; initialize everything here. The side your AI is
- * on (BLACK or WHITE) is passed in as "side". The constructor must finish 
- * within 30 seconds.
- */
 Player::Player(Side side) {
     // Will be set to true in test_minimax.cpp.
     testingMinimax = false;
@@ -15,24 +10,9 @@ Player::Player(Side side) {
     srand(time(NULL));
 }
 
-/*
- * Destructor for the player.
- */
 Player::~Player() {
 }
 
-/*
- * Compute the next move given the opponent's last move. Your AI is
- * expected to keep track of the board on its own. If this is the first move,
- * or if the opponent passed on the last move, then opponentsMove will be NULL.
- *
- * msLeft represents the time your AI has left for the total game, in
- * milliseconds. doMove() must take no longer than msLeft, or your AI will
- * be disqualified! An msLeft value of -1 indicates no time limit.
- *
- * The move returned must be legal; if there are no valid moves for your side,
- * return NULL.
- */
 Move *Player::doMove(Move *opponentsMove, int msLeft) {
     board.doMove(opponentsMove, opponent);
 
--- player.h
+++ player.h
@@ -7,23 +7,58 @@
 #include <ctime>
 #include "common.h"
 #include "board.h"
-using namespace std;
 
 class Player {
 
 public:
+
+    /**
+     * @brief Creates a new player playing the given side.
+     */
     Player(Side side);
+
+    /**
+     * @brief Cleans up all resources allocated for the player.
+     */
     ~Player();
     
+    /**
+     * @brief Performs the opponent's move and returns this player's move.
+     *
+     * Keeps `board` consistent with both moves.
+     *
+     * @param opponentsMove A pointer to the opponent's last move.
+     * @param msLeft The number of milliseconds allowed for this player, or -1
+     *               if there is no limit.
+     * 
+     * @return A pointer to this player's next move, or NULL if no valid move
+     *         is left.
+     */
     Move *doMove(Move *opponentsMove, int msLeft);
 
-    // Flag to tell if the player is running within the test_minimax context
+    /**
+     * @brief Flag to tell if the player is running within the test_minimax
+     *        context
+     */
     bool testingMinimax;
 
 private:
+
+    /**
+     * @brief This player's side.
+     */
     Side side;
+
+    /**
+     * @brief The other player's side.
+     */
     Side opponent;
+
+    /**
+     * @brief The board of play.
+     */
     Board board;
+
 };
 
 #endif
