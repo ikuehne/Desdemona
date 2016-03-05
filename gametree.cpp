c0f0c56ce3efb567108bece99479edd2134bb14c
ikuehne <ikuehne617@gmail.com>
2016-03-05 11:24:44 -0800

    Added working game tree implementation.

--- gametree.cpp
+++ gametree.cpp
@@ -5,72 +5,149 @@ GameTree::GameTree(Board *board, Side side, int level, bool testingMinimax) {
     this->board = board;
     this->side = side;
     this->testingMinimax = testingMinimax;
+    next = NULL;
+    originalLevel = level;
 
-    // If this is the bottom of the tree, make sure to properly indicate it.
-    // Set the worst score (for minimax) to the current score.
-    if (level == 0) {
-        next = NULL;
-        best_move = NULL;
-        worst_score = new int(board->assess(side, testingMinimax));
-        return;
-    }
+    addPly(level);
+}
 
-    next = new vector<pair<Move *, GameTree *> >();
+GameTree::GameTree(Board *board, Side side, int level,
+                   int originalLevel, bool testingMinimax) {
+    // Copy over some parameters.
+    this->board = board;
+    this->side = side;
+    this->testingMinimax = testingMinimax;
+    this->originalLevel = originalLevel;
+    next = NULL;
 
-    std::vector<Move *> *legals = board->getLegals(side);
-
-    if (legals->size() == 0) {
-        worst_score = new int(board->assess(side, testingMinimax));
-    }
-
-    Side other = side == WHITE? BLACK: WHITE;
-
-    Board *temp;
-    GameTree *next_tree;
-    for (int i = 0; i < legals->size(); i++) {
-        temp = board->copy();
-        temp->doMove((*legals)[i], side);
-        next_tree = new GameTree(temp, other,
-                                 level - 1, testingMinimax);
-        next->push_back(pair<Move *, GameTree *>((*legals)[i], next_tree));
-    }
-
-    best_move = NULL;
-    worst_score = NULL;
-    delete legals;
+    addPly(level);
 }
 
 GameTree::~GameTree() {
-    delete board;
+    if (next) {
+        // Clean up each of the subtrees,
+        for (int i = 0; i < next->size(); i++) {
+            delete (*next)[i].first;
+            delete (*next)[i].second;
+        }
+    }
+
+    // And then the obvious stuff.
     delete next;
-    delete worst_score;
-    delete best_move;
+    delete board;
 }
 
 int GameTree::assess() {
-    if (worst_score) return *worst_score;
+    if (next == NULL) {
+        return board->assess(side, testingMinimax);
+    }
+ 
+    if (next->size() == 0) {
+        return board->assess(side, testingMinimax);
+    }
 
     int curr_score;
-    int min_score = (*next)[0].second->assess();
-    int max_score = min_score;
-    Move *best_move = (*next)[0].first;
+    int max_score = -(*next)[0].second->assess();
 
     for (int i = 1; i < next->size(); i++) {
-        curr_score = (*next)[i].second->assess();
-        if (curr_score < min_score) min_score = curr_score;
+        curr_score = -(*next)[i].second->assess();
+        if (curr_score > max_score) max_score = curr_score;
+    }
 
-        if (curr_score > max_score) {
-            max_score = curr_score;
+    return max_score;
+}
+
+Move *GameTree::getBestMove() {
+    if (next == NULL || next->size() == 0) return NULL;   
+
+    int curr_score;
+    int best_score = -(*next)[0].second->assess();
+
+    Move *best_move = (*next)[0].first;
+    
+    for (int i = 1; i < next->size(); i++) {
+        curr_score = -(*next)[i].second->assess();
+        if (curr_score > best_score) {
+            best_score = curr_score;
             best_move = (*next)[i].first;
         }
     }
 
-    worst_score = new int(min_score);
-
-    return min_score;
+    return new Move(*best_move);
 }
 
-Move *GameTree::get_best_move() {
-    assess();
-    return best_move;
+void GameTree::addPly(int c) {
+    if (c == 0) return;
+
+    if (next == NULL) {
+        next = new vector<pair<Move *, GameTree *> >();
+        Side other = side == WHITE? BLACK: WHITE;
+        std::vector<Move *> *legals = board->getLegals(side);
+        Board *temp;
+        GameTree *next_tree;
+        for (int i = 0; i < legals->size(); i++) {
+            temp = board->copy();
+            temp->doMove((*legals)[i], side);
+            next_tree = new GameTree(temp,  other,
+                                     c - 1, originalLevel, testingMinimax);
+            next->push_back(pair<Move *, GameTree *>(new Move(*(*legals)[i]),
+                                                     next_tree));
+        }
+        for (int i = 0; i < legals->size(); i++) {
+            delete (*legals)[i];
+        }
+
+        delete legals;
+        return;
+    }
+
+    for (int i = 0; i < next->size(); i++) {
+        (*next)[i].second->addPly(c);
+    }
+}
+
+void GameTree::doMove(Move *move) {
+    if (move == NULL) {
+        side = side == WHITE? BLACK: WHITE;
+        delete next;
+        next = NULL;
+
+        addPly(originalLevel);
+        return;
+    }
+
+    if (next == NULL) {
+        board->doMove(move, side);
+        side = side == WHITE? BLACK: WHITE;
+
+        return;
+    }
+
+    for (int i = 0; i < next->size(); i++) {
+
+        // If the results of the move have already been calculated,
+        if (*move == *((*next)[i].first)) {
+            // Transfer a bunch of crap from that branch to this one,
+            GameTree *new_tree = (*next)[i].second;
+            vector<pair<Move *, GameTree *> > *old_next = next;
+            next = new_tree->next;
+            new_tree->next = NULL;
+
+            board = new_tree->board;
+            new_tree->board = NULL;
+            side = new_tree->side;
+
+            // And then clean it all up.
+            for (int j = 0; j < old_next->size(); j++) {
+                delete (*old_next)[j].first;
+                delete (*old_next)[j].second;
+            }
+            delete old_next;
+        }
+    }
+    addPly(1);
+}
+
+void GameTree::doOpponentMove(Move *move) {
+    doMove(move);
 }
--- gametree.h
+++ gametree.h
@@ -1,7 +1,9 @@
 #include <utility>
 #include <vector>
+#include <iostream>
 #include "common.h"
 #include "board.h"
+#include <cassert>
 
 using namespace std;
 
@@ -32,9 +34,49 @@ public:
     /**
      * @brief Get the best move from the current position.
      */
-    Move *get_best_move();
+    Move *getBestMove();
+
+    /**
+     * @brief Perform a move and await the response.
+     */
+    void doMove(Move *move);
+
+    /**
+     * @brief Perform the opponent's move and get up the next ply.
+     */
+    void doOpponentMove(Move *move);
+
 
 protected:
+
+    /**
+     * @brief Create a game tree and populate it to a given level. Utility
+     * constructor used to ensure every level knows what the original depth of
+     * the tree was.
+     *
+     * @param board A pointer to the current board.
+     * @param side The side to play for.
+     * @param level The ply to create the game tree to.
+     * @param originalLevel The level originally specified for the tree.
+     * @param testingMinimax Whether to use the simplified heuristic for
+     *                       testing Minimax.
+     *
+     */
+    GameTree(Board *board, Side side, int level, int originalLevel, 
+             bool testingMinimax);
+
+    /**
+     * @brief The original depth of the tree.
+     */
+    int startingdepth;
+
+    /**
+     * @brief Add new levels to the game tree.
+     *
+     * @param c The number of levels to add.
+     */
+    void addPly(int c);
+
     /**
      * @brief Keep track of all possible moves from this point and the
      *        resulting trees.
@@ -69,4 +111,9 @@ protected:
      * @brief Whether the tree is being used to test minimax.
      */
     bool testingMinimax;
+
+    /**
+     * @brief The original level of the tree.
+     */
+    int originalLevel;
 };
